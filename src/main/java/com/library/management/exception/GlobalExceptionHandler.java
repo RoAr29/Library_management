208b6575fe88d5f719e8a4d8167167afcde6f941
Srushti <agrawalsp_2@rknec.edu>
2026-07-20 19:24:51 +0530

    ..

---
+++ src/main/java/com/library/management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.library.management.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
--- src/main/java/com/library/management/service/BookService.java
+++ src/main/java/com/library/management/service/BookService.java
@@ -1,35 +1,48 @@
 package com.library.management.service;
 
 import java.util.*;
+
+import com.library.management.exception.BookNotFoundException;
 import com.library.management.model.Book;
 import com.library.management.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository repository;
-    
-    public void addBook(Book book) {
-    	repository.addBook(book);
-    }
-    
-    public List<Book> getAllBooks(){
-    	return repository.getAllBooks();
-    }
-    
-    public Book getBookById(int id) {
-    	
-    	List<Book> books = repository.getAllBooks();
-    	
-    	for(Book book:books) {
-    		if(book.getId() == id) {
-    			return book;
-    		}
-    	}
-    	return null;
+
+    public Book addBook(Book book) {
+        return repository.save(book);
     }
 
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    public Book getBookById(int id) {
+    	return repository.findById(id).orElseThrow(() -> new BookNotFoundException("Book with id " + id + " not found"));
+    }
+
+    public Book updateBook(int id, Book updatedBook) {
+
+        Book existingBook = repository.findById(id).orElse(null);
+
+        if (existingBook == null) {
+            return null;
+        }
+
+        existingBook.setTitle(updatedBook.getTitle());
+        existingBook.setAuthor(updatedBook.getAuthor());
+        existingBook.setPrice(updatedBook.getPrice());
+
+        return repository.save(existingBook);
+    }
+
+    public void deleteBook(int id) {
+        repository.deleteById(id);
+    }
 }
