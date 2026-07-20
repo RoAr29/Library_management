f2a2427eba1cd85b533429ef5012258afd91e588
Srushti <agrawalsp_2@rknec.edu>
2026-07-20 19:42:07 +0530

    Handling exceptional errors

--- src/main/java/com/library/management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/library/management/exception/GlobalExceptionHandler.java
@@ -1,8 +1,21 @@
 package com.library.management.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<String> handleBookNotFound(BookNotFoundException ex){
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND
+        );
+
+    }
 
 }
