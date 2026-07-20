6ea9432a28c020159149c1cccb7aef961277b70d
Srushti <agrawalsp_2@rknec.edu>
2026-07-20 17:05:55 +0530

    ..

--- src/main/java/com/library/management/model/Book.java
+++ src/main/java/com/library/management/model/Book.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
+
 @Entity
 @Table(name = "books")
 public class Book {
