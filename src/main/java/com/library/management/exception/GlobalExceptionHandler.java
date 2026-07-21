b90d6f57c1c7120cbb7a8f3b259d9c8f58c174e2
Srushti <agrawalsp_2@rknec.edu>
2026-07-21 23:23:00 +0530

    ..

--- src/main/java/com/library/management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/library/management/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.library.management.exception;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,7 +25,27 @@ public class GlobalExceptionHandler {
 
 	    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationException(
+	        MethodArgumentNotValidException ex) {
+
+	    Map<String, String> errors = new HashMap<>();
+
+	    for(FieldError error : ex.getBindingResult().getFieldErrors()){
+
+	        errors.put(
+	                error.getField(),
+	                error.getDefaultMessage()
+	        );
+
+	    }
+
+	    return new ResponseEntity<>(
+	            errors,
+	            HttpStatus.BAD_REQUEST
+	    );
+
+	}
 
     }
-
-}
